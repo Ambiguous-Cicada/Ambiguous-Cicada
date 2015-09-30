39d3e7ec5ce2d85080bd346bc25983a59969f241
Michael Junio <michael.ray.junio@gmail.com>
2015-09-30 13:14:34 -0700

    Progress on socket chats

--- client/loading.js
+++ client/loading.js
@@ -1,56 +1,62 @@
 angular.module('kwiki.load', [])
 
-.factory('LoadFactory', ['$http', '$timeout', '$location', 'socket', function ($http, $timeout, $location, socket) {
+.factory('LoadFactory', ['$location', 'socket', '$window', function ($location, socket, $window) {
   var loadFac = {};
 
-  loadFac.socket = socket.connect("/match");
-
-  loadFac.getMatch = function () {
-    return $http({
-      method: 'GET',
-      url: '/match'
-    });
-  };
-
   loadFac.chatId = null;
-
-  loadFac.checkMatch = function () {
-    loadFac.getMatch().then(function (res, err) {
-      if(res.data.chatId) {
-        loadFac.chatId = res.data.chatId;
-        $location.path('/chat');
-      } else {
-        $timeout(function() {
-          loadFac.checkMatch();
-        }, 2000);
-      }
-    });
-  };
+  loadFac.socket = socket.connect("match");
 
   loadFac.postMatch = function () {
-    return $http({
-      method: 'POST',
-      url: '/match'
-      // data: location data in the future
-    })
-    .then(function (res) {
-        if (res.status === 201){
-          loadFac.checkMatch();
-        }
+    // use socket to emit 
+    console.log('postMatch wascalled');
+    console.log(this.socket);
+    // $window.localStorage.debug = '*';
+    this.socket.emit('matching', 'sup'); // sending obj with name/id prop
+    this.socket.on('matched', function (data) {
+      loadFac.chatId = data;
+      $location.path('/chat');
     });
   };
-
   return loadFac;
+    
+    // return $http({
+    //   method: 'POST',
+    //   url: '/match'
+    //   // data: location data in the future
+    // })
+    // .then(function (res) {
+    //     if (res.status === 201){
+    //       loadFac.checkMatch();
+    //     }
+    // });
 
+  // loadFac.getMatch = function () {
+  //   return $http({
+  //     method: 'GET',
+  //     url: '/match'
+  //   });
+  // };
+
+
+  // loadFac.checkMatch = function () {
+  //   loadFac.getMatch().then(function (res, err) {
+  //     if(res.data.chatId) {
+        // loadFac.chatId = res.data.chatId;
+        // $location.path('/chat');
+  //     } else {
+  //       $timeout(function() {
+  //         loadFac.checkMatch();
+  //       }, 2000);
+  //     }
+  //   });
+  // };
 }])
 
-.controller('LoadController', ['$scope', '$http', 'LoadFactory', 'Users', function ($scope, $http, LoadFactory, Users) {
+.controller('LoadController', ['$scope', 'LoadFactory', 'Users', function ($scope, LoadFactory, Users) {
   $scope.buttonDisabled = false;
   $scope.submit = function () {
     $scope.buttonDisabled = true;
-    LoadFactory.socket;
     LoadFactory.postMatch();
-
   };
 
   $scope.logOut = function () {
