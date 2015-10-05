960070d752b8e1eead34e285f6dbb8c65fd05fbd
John T Knox <JTKnox@Johns-MacBook-Pro.local>
2015-10-05 10:08:41 -0700

    Update client-mobile

--- client-mobile/www/js/socket.js
+++ client-mobile/www/js/socket.js
@@ -3,8 +3,7 @@ angular.module('kwiki.socket', [])
 .factory('SocketFactory', ['$location', function ($location) {
   var socketFact = {};
 
-  //hacky way to make this work in developer environments at specified port number
-  socketFact.host = $location.host() !== "localhost" ? $location.host() : "localhost:3000";
+  socketFact.host = $location.host() + ":8000";
 
   socketFact.connect = function (nameSpace) {
     if (!nameSpace) {
