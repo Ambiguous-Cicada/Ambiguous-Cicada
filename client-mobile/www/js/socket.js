f8ee0633f109072f01928f0a8c6f518915273a70
John T Knox <JTKnox@Johns-MacBook-Pro.local>
2015-10-05 09:13:59 -0700

    close #14 fix merge conflicts

--- client-mobile/www/js/socket.js
+++ client-mobile/www/js/socket.js
@@ -3,12 +3,8 @@ angular.module('kwiki.socket', [])
 .factory('SocketFactory', ['$location', function ($location) {
   var socketFact = {};
 
-<<<<<<< HEAD
-  socketFact.host = $location.host() + ":8000";
-=======
   //hacky way to make this work in developer environments at specified port number
   socketFact.host = $location.host() !== "localhost" ? $location.host() : "localhost:3000";
->>>>>>> 36bec6a8a6c517934c8ce862b31f95b75363fce5
 
   socketFact.connect = function (nameSpace) {
     if (!nameSpace) {
